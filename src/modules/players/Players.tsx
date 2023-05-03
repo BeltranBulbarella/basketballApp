06e67de8f273c2d2f7ca095a401a5ed969a53948
beltranbulbarella <beltran.bulbarella@ing.austral.edu.ar>
2023-05-03 18:45:21 -0300

    feat: added players tab

--- src/modules/players/Players.tsx
+++ src/modules/players/Players.tsx
@@ -1,12 +1,95 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+const mockData = [
+  {
+    id: 1,
+    team: 'Team 1',
+    points: 10,
+    ppm: 10,
+    mvp: 10,
+  },
+  {
+    id: 2,
+    team: 'Team 2',
+    points: 10,
+    ppm: 10,
+    mvp: 10,
+  },
+];
 
 const Players = ({navigation}: {navigation: any}) => {
   return (
     <View>
-      <Text>Players</Text>
+      <Text style={styles.title}>Players</Text>
+      <View style={styles.container}>
+        <Text style={styles.subtitleNumber}>#</Text>
+        <Text style={styles.subtitleTeam}>Team</Text>
+        <Text style={styles.subtitle}>P</Text>
+        <Text style={styles.subtitle}>PPM</Text>
+        <Text style={styles.subtitle}>MVP</Text>
+      </View>
+      {mockData.map((data, index) => {
+        return (
+          <View style={[styles.container, {marginTop: 10}]} key={index}>
+            <Text style={styles.mapSubtitleNumber}>{data.id}</Text>
+            <Text style={styles.mapSubtitleTeam}>{data.team}</Text>
+            <Text style={styles.mapSubtitle}>{data.points}</Text>
+            <Text style={styles.mapSubtitle}>{data.ppm}</Text>
+            <Text style={styles.mapSubtitle}>{data.mvp}</Text>
+          </View>
+        );
+      })}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    width: 211,
+    height: 50,
+    fontWeight: 'bold',
+    fontSize: 32,
+    lineHeight: 39,
+    padding: 5,
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    flex: 1,
+  },
+  subtitleNumber: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    flex: 0.5,
+  },
+  subtitleTeam: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    flex: 2,
+  },
+  mapSubtitleNumber: {
+    fontSize: 20,
+    textAlign: 'center',
+    flex: 0.5,
+  },
+  mapSubtitleTeam: {
+    fontSize: 16,
+    textAlign: 'center',
+    flex: 2,
+  },
+  mapSubtitle: {
+    fontSize: 16,
+    textAlign: 'center',
+    flex: 1,
+  },
+});
 
 export default Players;
--- src/modules/standings/Standings.tsx
+++ src/modules/standings/Standings.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const Standings = ({navigation}: {navigation: any}) => {
   return (
     <View>
-      <Text>Standings</Text>
+      <Text style={styles.title}>Standings</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  title: {
+    width: 211,
+    height: 50,
+    fontWeight: 'bold',
+    fontSize: 32,
+    lineHeight: 35,
+    padding: 5,
+  },
+});
 
 export default Standings;
