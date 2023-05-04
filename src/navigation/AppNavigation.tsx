34cf20c1914c6dab9325611acc9f1637017f4d27
beltranbulbarella <beltran.bulbarella@ing.austral.edu.ar>
2023-05-04 10:44:27 -0300

    feat: finished jest tests

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -23,27 +23,27 @@ export default AppNavigation;
 
 {
   /*
-<NavigationContainer>
-          <Stack.Navigator initialRouteName="Home" screenOptions={{animationTypeForReplace:'pop'}}>
-              <Stack.Screen name="Home" component={HomeScreen} options={() => ({
-                  title: "",
-                  headerStyle: {
-                      backgroundColor:colorPalette.tertiary
-                  },
-                  animationTypeForReplace: 'push',
-                  animation:'slide_from_right'
-              })}
-              />
-              <Stack.Screen name="AddAlarm" component={AddAlarmScreen} options={() => ({
-                  title: "",
-                  headerStyle: {
-                      backgroundColor:colorPalette.tertiary
-                  },
-                  animationTypeForReplace: 'push',
-                  animation:'slide_from_right'
-              })}
-              />
-          </Stack.Navigator>
-      </NavigationContainer>
- */
+  <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home" screenOptions={{animationTypeForReplace:'pop'}}>
+                <Stack.Screen name="Home" component={HomeScreen} options={() => ({
+                    title: "",
+                    headerStyle: {
+                        backgroundColor:colorPalette.tertiary
+                    },
+                    animationTypeForReplace: 'push',
+                    animation:'slide_from_right'
+                })}
+                />
+                <Stack.Screen name="AddAlarm" component={AddAlarmScreen} options={() => ({
+                    title: "",
+                    headerStyle: {
+                        backgroundColor:colorPalette.tertiary
+                    },
+                    animationTypeForReplace: 'push',
+                    animation:'slide_from_right'
+                })}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+   */
 }
